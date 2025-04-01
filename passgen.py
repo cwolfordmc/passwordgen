71b3a83a29c216385155ddfcf6ce71a107d959ff
cwolfordmc <christopher_m_wolford@progressive.com>
2025-04-01 09:05:49 -0400

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -5,8 +5,8 @@ def gen_pass():
     #Vars needed for password chars
     length = int(16)
     chars = string.ascii_letters
-    digits = chars + string.digits 
-    syms = chars + string.punctuation
+    digits = string.digits 
+    syms = string.punctuation
     #Build Password
     password = ''.join(random.choice(chars + digits + syms) for _ in range(length))
     return password
