4cfdb3a47702d2178e3d7a89c1a2a5cbd688bbd0
cwolfordmc <christopher_m_wolford@progressive.com>
2025-04-01 08:40:42 -0400

    Create passgen.py

---
+++ passgen.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+def gen_pass():
+    #Vars needed for password chars
+    length = int(16)
+    chars = string.ascii_letters
+    digits = chars + string.digits 
+    syms = chars + string.punctuation
+    #Build Password
+    password = ''.join(random.choice(chars + digits + syms) for _ in range(length))
+    return password
+
+password = gen_pass()
+print("New Pass:", password)
